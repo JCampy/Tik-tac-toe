22c946945708a4b2f5d1a252c39ec290f10df1a8
JCampy <93626334+JCampy@users.noreply.github.com>
2023-12-14 10:40:36 -0600

    Initial commit of main

---
+++ src/main/java/TikTacToe/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package TikTacToe;
+import java.util.*;
+
+/**
+ *
+ * @author JCampy
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Board bs = new Board();
+        bs.makeBoard(); 
+        
+        System.out.println(bs.getSpecificTile(0, 0));
+        // bs.setBoard(0,0,10);
+        bs.printBoard();
+       
+        
+    }
+    
+}
